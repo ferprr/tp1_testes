022b7abaaf7d46f0e2ce6cdf407aa8ef623fcf72
Etelvina <etelvina.oliveira2003@gmail.com>
2023-11-02 20:35:20 -0300

    Add postDetail and postNew

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.post_list, name='postList'),
+  path('', views.postList, name='postList'),
+  path('post/<int:pk>/', views.postDetail, name='postDetail'),
+  path('post/new/', views.postNew, name='postNew')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
+from .forms import PostForm
 
-def post_list(request):
+def postList(request):
   posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date') 
   return render(request, 'blog/postList.html', {'posts': posts})
+
+def postDetail(request, pk):
+  post = get_object_or_404(Post, pk=pk)
+  return render(request, 'blog/postDetail.html', {'post': post})
+
+def postNew(request):
+  form = PostForm()
+  return render(request, 'blog/postNew.html', {'form': form})
