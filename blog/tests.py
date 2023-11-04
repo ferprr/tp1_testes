30d0a8e2cc0afab423ccc361a062023892582fad
Etelvina <etelvina.oliveira2003@gmail.com>
2023-11-04 02:41:56 -0300

    Add tests

--- blog/tests.py
+++ blog/tests.py
@@ -16,10 +16,6 @@ class PostModelTest(TestCase):
         )
         self.post.category.add(self.category)
 
-    # def test_category_creation(self):
-    #     category = Category.objects.get(name="Technology")
-    #     self.assertEqual(category.name, "Technology")
-
     def test_post_creation(self):
         post = Post.objects.get(title="Post 1")
         self.assertEqual(post.author, self.user)
@@ -40,22 +36,6 @@ class PostModelTest(TestCase):
     def test_filter_posts_by_category(self):
         posts_in_technology_category = Post.objects.filter(category__name="Technology")
         self.assertEqual(posts_in_technology_category.count(), 1)
-
-    # def test_filter_posts_by_author(self):
-    #     user_posts = Post.objects.filter(author=self.user)
-    #     self.assertEqual(user_posts.count(), 1)
-
-    # def test_update_post(self):
-    #     post = Post.objects.get(title="Post 1")
-    #     post.title = "Post atualizado"
-    #     post.save()
-    #     updated_post = Post.objects.get(pk=post.pk)
-    #     self.assertEqual(updated_post.title, "Post atualizado")
-
-    # def test_delete_category(self):
-    #     category = Category.objects.get(name="Technology")
-    #     category.delete()
-    #     self.assertFalse(Category.objects.filter(name="Technology").exists())
 
     def test_delete_post(self):
         post = Post.objects.get(title="Post 1")
@@ -135,25 +115,24 @@ class PostModelTest(TestCase):
     def test_postRemove_view_invalid_post(self):
         self.client.login(username='testuser', password='testpassword')
         response = self.client.get(reverse('postRemove', args=[999]))  # Assuming 999 is an invalid post ID
-        self.ass(response.status_code, 302)
+        self.assertEqual(response.status_code, 302)
 
-    # def test_login_form_valid(self):
-    #     form = LoginForm(data={'username': 'testuser', 'password': 'Test123password'})
-    #     self.assertTrue(form.is_valid())
+    def test_login_form_valid(self):
+        form = LoginForm(data = {'username': 'testUser', 'password': 'password'})
+        self.assertTrue(form.is_valid())
 
     def test_login_form_invalid(self):
         form = LoginForm(data={'username': 'testuser'})
         self.assertFalse(form.is_valid())
 
-    # def test_post_form_valid(self):
-    #     form_data = {
-    #         'title': 'Test Title',
-    #         'subtitle': 'Test Subtitle',
-    #         'category': 'Test Category',
-    #         'text': 'Test Text',
-    #     }
-    #     form = PostForm(data=form_data)
-    #     self.assertTrue(form.is_valid())
+    def test_post_form_valid(self):
+        form_data = {
+            'title': 'Test Title',
+            'subtitle': 'Test Subtitle',
+            'text': 'Test Text',
+        }
+        form = PostForm(data=form_data)
+        self.assertTrue(form.is_valid())
 
     def test_post_form_invalid(self):
         form_data = {
@@ -165,15 +144,15 @@ class PostModelTest(TestCase):
         form = PostForm(data=form_data)
         self.assertFalse(form.is_valid())
 
-    # def test_registration_form_valid(self):
-    #     form_data = {
-    #         'username': 'testuser',
-    #         'email': 'test@example.com',
-    #         'password1': 'Testpassword123',
-    #         'password2': 'Testpassword123',
-    #     }
-    #     form = RegistrationForm(data=form_data)
-    #     self.assertTrue(form.is_valid())
+    def test_registration_form_valid(self):
+        form_data = {
+            'username': 'user789',
+            'email': 'test@example.com',
+            'password1': 'Testpassword123',
+            'password2': 'Testpassword123',
+        }
+        form = RegistrationForm(data=form_data)
+        self.assertTrue(form.is_valid())
 
     def test_registration_form_password_mismatch(self):
         form_data = {
@@ -196,17 +175,6 @@ class PostModelTest(TestCase):
         form = RegistrationForm(data=form_data)
         self.assertFalse(form.is_valid())
 
-    # def test_registration_form_existing_email(self):
-    #     User.objects.create_user(username='testuser', email='existing@example.com', password='testpassword')
-    #     form_data = {
-    #         'username': 'newuser',
-    #         'email': 'existing@example.com',
-    #         'password1': 'testpassword123',
-    #         'password2': 'testpassword123',
-    #     }
-    #     form = RegistrationForm(data=form_data)
-    #     self.assertFalse(form.is_valid())
-
     def test_post_form_with_empty_data(self):
         form = PostForm(data={})
         self.assertFalse(form.is_valid())
@@ -215,46 +183,54 @@ class PostModelTest(TestCase):
         form = LoginForm(data={})
         self.assertFalse(form.is_valid())
 
-    def testViewUrlExistsAtDesiredLocation(self):
+    def test_view_url_exists_at_desired_location(self):
       response = self.client.get('/')
       self.assertEqual(response.status_code, 200)
 
-    def testViewUrlAccessibleByName(self):
+    def test_view_url_accessible_by_name(self):
       response = self.client.get(reverse('postList'))
       self.assertEqual(response.status_code, 200)
 
-    def testViewUsesCorrectTemplate(self):
+    def test_view_uses_correct_template_post_list(self):
       response = self.client.get(reverse('postList'))
       self.assertTemplateUsed(response, 'blog/postList.html')
+     
+    def test_view_uses_correct_template_post_publish(self):
+      response = self.client.get(reverse('postPublish'))
+      self.assertTemplateUsed(response, 'blog/postList.html')
 
-      #url invalida 404
+    def test_view_uses_correct_template_post_remove(self):
+      response = self.client.get(reverse('postRemove'))
+      self.assertTemplateUsed(response, 'blog/postList.html')
 
-    # def testViewUsesCorrectTemplate(self):
-    #   response = self.client.get(reverse('postPublish'))
-    #   self.assertTemplateUsed(response, 'blog/postList.html')
-
-    # def testViewUsesCorrectTemplate(self):
-    #   response = self.client.get(reverse('postRemove'))
-    #   self.assertTemplateUsed(response, 'blog/postList.html')
-
-    # def testViewUsesCorrectTemplate(self):
-    #   response = self.client.get(reverse('register'))
-    #   self.assertTemplateUsed(response, 'blog/register.html')
-
-    # def testViewUsesCorrectTemplate(self):
-    #   response = self.client.get(reverse('postDetail'))
-    #   self.assertTemplateUsed(response, 'blog/postDetail.html')
-
-    # def testViewDisplaysPosts(self):
-    #   response = self.client.get(reverse('postNew'))
-    #   self.assertContains(response, 'Título de Teste')
+    def test_view_uses_correct_template_register(self):
+      response = self.client.get(reverse('register'))
+      self.assertTemplateUsed(response, 'registration/register.html')
     
-    def testCreatedAtLabel(self):
-      createdAtLabel = self.post._meta.get_field('created_date').verbose_name
-      self.assertEquals(createdAtLabel, 'created date')
+    def test_max_length_title(self):
+        max_length = self.post._meta.get_field('title').max_length
+        self.assertEquals(max_length, 200)
 
-    def testPublishedDate(self):
-      publishedDateLabel = self.post._meta.get_field('published_date').verbose_name
-      self.assertEquals(publishedDateLabel, 'published date')
+    def test_author_label(self):
+        author_label = self.post._meta.get_field('author').verbose_name
+        self.assertEquals(author_label, 'author')
 
+    def test_title_label(self):
+        title_label = self.post._meta.get_field('title').verbose_name
+        self.assertEquals(title_label, 'title')
 
+    def test_subtitle_label(self):
+        subtitle_label = self.post._meta.get_field('subtitle').verbose_name
+        self.assertEquals(subtitle_label, 'subtitle')
+
+    def test_text_label(self):
+        text_label = self.post._meta.get_field('text').verbose_name
+        self.assertEquals(text_label, 'text')
+    
+    def test_created_at_label(self):
+        created_at_label = self.post._meta.get_field('created_date').verbose_name
+        self.assertEquals(created_at_label, 'created date')
+
+    def test_published_date(self):
+        published_date_label = self.post._meta.get_field('published_date').verbose_name
+        self.assertEquals(published_date_label, 'published date')
